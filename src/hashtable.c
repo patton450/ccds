d2bd21667892beb8bad0ab6db8905b1c6dfe59b5
patton450 <patton.450@osu.edu>
2020-11-06 16:21:57 -0500

    Started implamenting

---
+++ src/hashtable.c
@@ -0,0 +1,21 @@
+#include "hashtable.h"
+
+hashtable * hashtable_new(size_t cap, size_t (*hash)(void *), memcfg * mem, ccds_error * e){
+      
+}
+
+void hashtable_free(hastbale * ht, ccds_eeor * e){
+
+}
+
+size_t hashtable_length(hashtable * ht, ccds_error * e) {
+
+}
+
+bool hashtable_add(hashtable * ht, void * key, void * val, cccds_error * e){
+
+}
+
+void * hahstable_get(hashtable * ht, void * key. ccds_error * e) {
+
+}
--- src/hashtable.h
+++ src/hashtable.h
@@ -1,8 +1,16 @@
 #ifndef CCDS_HASHTABLE_H
 #define CCDS_HASHTABLE_H
 
+#include "list.h"
+#include "mem.h"
+#include "error.h"
+#include "log.h"
+#include "util.h"
+
 struct _hashtable {
     size_t (*hash)(void *)      hash_fn;
+    array *                     rep;        //Array of lists
+    size_t                      length;     //Total number of Key-Val Pairs, can be >= rep->cap
 };
 
 typedef struct _hashtable hashtable;
@@ -12,11 +20,11 @@ void            hashtable_free(hashtable * ht, ccds_error * e);
 size_t          hashtable_length(hashtable * ht, ccds_error * e);
 
 
-bool hashtable_try_add(hashtable * ht, void * key, void * val, ccds_error * e);
-void * hashtable_try_get(hashtable * ht, void * key, ccds_error * e);
-void * hashtable_try_remove(hashatable * ht, void * key, ccds_error * e);
+bool hashtable_add(hashtable * ht, void * key, void * val, ccds_error * e);
+void * hashtable_get(hashtable * ht, void * key, ccds_error * e);
+void * hashtable_remove(hashatable * ht, void * key, ccds_error * e);
 
 
-void hashtable_keys ()
+void hashtable_keys(hashtable * h, void ** keys_out, size_t ko_len, ccds_error * e);
 
 #endif
